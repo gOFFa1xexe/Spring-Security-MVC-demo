1544f38219ade50295c08d92bc9df6133b19a44f
gOFFa <lol228gofa@gmail.com>
2023-11-18 18:21:53 +0200

    Spring Security demo

---
+++ src/main/java/com/goffa/springboot/demoSecurity/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.goffa.springboot.demoSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+        return "fancy-login";
+    }
+    @GetMapping("/access-denied")
+    public String showDeniedAccess(){
+        return "access-denied";
+    }
+}
